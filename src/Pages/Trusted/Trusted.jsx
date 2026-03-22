08e2ee0742f2bc7b1272be536ba07f905b6ae052
arafat3029 <mdarafat12890@gmail.com>
2026-03-22 12:45:04 +0600

    add Trusted section

--- src/Pages/Trusted/Trusted.jsx
+++ src/Pages/Trusted/Trusted.jsx
@@ -2,10 +2,34 @@ import React from 'react';
 
 const Trusted = () => {
     return (
-        <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi vel vitae cum ea pariatur. A unde ratione ullam libero deleniti, facilis nobis? Dicta, quod harum! Totam dolore incidunt perspiciatis provident.</p>
+        <div className='bg-gradient-to-r from-[#632EE3] to-[#9F62F2] h-64 w-full'>
+            <div className='flex items-center justify-center'>
+                <h3 className='text-white text-5xl mt-9 font-bold'>Trusted by Millions, Built for You</h3>
+            </div>
+
+            <div className='flex text-white justify-center gap-40 mt-7'>
+
+                <div className='flex flex-col justify-center items-center text-center'>
+                    <span>Total Downloads</span>
+                    <h3 className='font-bold text-4xl mt-1.5'>29.6M</h3>
+                    <p>21% more than last month</p>
+                </div>
+
+                <div className='flex flex-col justify-center items-center text-center'>
+                    <span>Total Reviews</span>
+                    <h3 className='font-bold text-4xl mt-1.5'>906K</h3>
+                    <p>46% more than last month</p>
+                </div>
+
+                <div className='flex flex-col justify-center items-center text-center'>
+                    <span>Active Apps</span>
+                    <h3 className='font-bold text-4xl mt-1.5'>132+</h3>
+                    <p className='mt-4'>46% more than last month</p>
+                </div>
+
+            </div>
         </div>
     );
 };
 
-export default Trusted;<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi vel vitae cum ea pariatur. A unde ratione ullam libero deleniti, facilis nobis? Dicta, quod harum! Totam dolore incidunt perspiciatis provident.</p>
+export default Trusted;
